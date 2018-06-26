6c5c99102ef0ed3bffec39d9bf10779a3c162e6d
İbrahim Özdemir <ibrahim.ozdemir@outlook.com.tr>
2018-06-26 21:36:51 +0300

    Twitter done

--- Embeder.js
+++ Embeder.js
@@ -4,29 +4,31 @@ function DefineSite(domainID, url, height, width, border, allowFullScreen) {
     width = width === undefined ? 560 : width;
     border = border === undefined ? false : border;
     allowFullScreen = allowFullScreen === undefined ? true : allowFullScreen;
-    //
-
-    // 
-    var str = url.split(".");
-    str.forEach(element => {
-        if (element === "youtube" || element === "youtu") {
-            return YoutubeLink(domainID, url, height, width, border, allowFullScreen);
-        }
-    });
-    return 0;
-}
-
-function YoutubeLink(domainID, url, height, width, border, allowFullScreen) {
-    var embed = "<iframe";
-    var str = url.split("=");
-    var link = str[str.length - 1];
     var screen = "";
     if (border === false)
         border = 0;
     else
         border = 1;
-    if(allowFullScreen === true)
+    if (allowFullScreen === true)
         screen = "allowfullscreen";
+    // 
+    var str = url.split(".");
+    str.forEach(element => {
+        if (element === "youtube" || element === "youtu" || element === "https://youtube" || element === "https://youtu") {
+            return YoutubeLink(domainID, url, height, width, border, screen);
+        }
+    });
+    str.forEach(element => {
+        if (element === "https://twitter")
+            return TwitterLink(domainID, url);
+    });
+}
+
+function YoutubeLink(domainID, url, height, width, border, screen) {
+    var embed = "<iframe";
+    var str = url.split("=");
+    var link = str[str.length - 1];
+
     //width
     embed += " width=\"" + width + "\"";
     //height
@@ -44,7 +46,18 @@ function YoutubeLink(domainID, url, height, width, border, allowFullScreen) {
     document.getElementById(domainID).innerHTML = embed;
 }
 
-function TwitterLink(domainID, url, height, width, border, allowFullScreen){
-    
+function TwitterLink(domainID, url) {
+    var script = document.createElement('script');
+    script.src = "https://platform.twitter.com/widgets.js";
+    script.async = true;
+    script.charset = "utf-8"
+    document.head.appendChild(script);
+    var embed = "<blockquote class=\"twitter-tweet\"><a href=\"https://twitter.com/i/status/";
+    var str = url.split("/");
+    var link = str[str.length - 1];
+    embed += link;
+    embed += "?ref_src=twsrc%5Etfw\"></a></blockquote>";
+
+    document.getElementById(domainID).innerHTML = embed;
 }
 
--- EmbederUsage.js
+++ EmbederUsage.js
@@ -1,1 +1,2 @@
-DefineSite("example1","https://www.youtube.com/watch?v=3sMkI7BluMQ",250,250,true,true);
+DefineSite("example1","https://youtube.com/watch?v=3sMkI7BluMQ",250,250,true,true);
+DefineSite("example2","https://twitter.com/i/status/803861418789335040");
