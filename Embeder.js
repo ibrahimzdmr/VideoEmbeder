724a7ddc94b7167f5eb67543403b117acca20125
İbrahim Özdemir <ibrahim.ozdemir@outlook.com.tr>
2018-06-28 22:26:42 +0300

    almost vimeo added

--- Embeder.js
+++ Embeder.js
@@ -1,4 +1,22 @@
-function DefineSite(domainID, url, height, width, border, allowFullScreen) {
+function DefineSite(domainID, url) {
+    var str = url.split(".");
+    str.forEach(element => {
+        if (element === "youtube" || element === "youtu" || element === "https://youtube" || element === "https://youtu") {
+            return YoutubeLink(domainID, url);
+        }
+    });
+    str.forEach(element => {
+        if (element === "https://twitter")
+            return TwitterLink(domainID, url);
+    });
+    str.forEach(element => {
+        if (element === "vimeo" || element === "https://vimeo")
+            return VimeoLink(domainID, url);
+    })
+
+}
+
+function YoutubeLink(domainID, url, height, width, border, allowFullScreen) {
     //Default values
     height = height === undefined ? 315 : height;
     width = width === undefined ? 560 : width;
@@ -12,23 +30,9 @@ function DefineSite(domainID, url, height, width, border, allowFullScreen) {
     if (allowFullScreen === true)
         screen = "allowfullscreen";
     // 
-    var str = url.split(".");
-    str.forEach(element => {
-        if (element === "youtube" || element === "youtu" || element === "https://youtube" || element === "https://youtu") {
-            return YoutubeLink(domainID, url, height, width, border, screen);
-        }
-    });
-    str.forEach(element => {
-        if (element === "https://twitter")
-            return TwitterLink(domainID, url);
-    });
-}
-
-function YoutubeLink(domainID, url, height, width, border, screen) {
     var embed = "<iframe";
     var str = url.split("=");
     var link = str[str.length - 1];
-
     //width
     embed += " width=\"" + width + "\"";
     //height
@@ -53,12 +57,42 @@ function TwitterLink(domainID, url) {
     script.async = true;
     script.charset = "utf-8"
     document.head.appendChild(script);
+
     var embed = "<blockquote class=\"twitter-tweet\"><a href=\"https://twitter.com/i/status/";
     var str = url.split("/");
     var link = str[str.length - 1];
     embed += link;
     embed += "?ref_src=twsrc%5Etfw\"></a></blockquote>";
-
     document.getElementById(domainID).innerHTML = embed;
 }
 
+
+function VimeoLink(domainID, url, width, height, border, allowFullScreen) {
+    height = height === undefined ? 360 : height;
+    width = width === undefined ? 640 : width;
+    border = border === undefined ? false : border;
+    allowFullScreen = allowFullScreen === undefined ? true : allowFullScreen;
+    var screen = "";
+    if (border === false)
+        border = 0;
+    else
+        border = 1;
+    if (allowFullScreen === true)
+        screen = "webkitallowfullscreen mozallowfullscreen allowfullscreen";
+    // 
+    var embed = "<iframe src=\"https://player.vimeo.com/video/";
+    var str = url.split("/");
+    var link = str[str.length - 1];
+    //link
+    embed += link;
+    //width
+    embed += "\" width=\"" + width + "\"";
+    //height
+    embed += " height=\"" + height + "\"";
+    // frameborder
+    embed += " frameborder=\"" + border + "\"";
+    // allowfullscreen
+    embed += screen;
+    embed += "></iframe>";
+    document.getElementById(domainID).innerHTML = embed;
+}
--- EmbederUsage.js
+++ EmbederUsage.js
@@ -1,2 +1,3 @@
-DefineSite("example1","https://youtube.com/watch?v=3sMkI7BluMQ",250,250,true,true);
+DefineSite("example1","https://youtube.com/watch?v=3sMkI7BluMQ");
 DefineSite("example2","https://twitter.com/i/status/803861418789335040");
+DefineSite("example3","https://vimeo.com/259539583");
