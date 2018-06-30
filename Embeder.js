ec0ecc2253ae6a920b0ff36c64f995ef89c0c25d
İbrahim Özdemir <ibrahim.ozdemir@outlook.com.tr>
2018-06-30 21:16:16 +0300

    facebook added

--- Embeder.js
+++ Embeder.js
@@ -1,7 +1,7 @@
 function DefineSite(domainID, url) {
     var str = url.split(".");
     str.forEach(element => {
-        if (element === "youtube"  || element === "youtu" || element === "https://youtube" || element === "https://youtu") {
+        if (element === "youtube" || element === "youtu" || element === "https://youtube" || element === "https://youtu") {
             return YoutubeLink(domainID, url);
         }
     });
@@ -95,8 +95,34 @@ function VimeoLink(domainID, url, width, height, border, allowFullScreen) {
     embed += screen;
     embed += "></iframe>";
     document.getElementById(domainID).innerHTML = embed;
+
 }
 
-function FacebookLink(domainID,url,width,height,border,overflow,scrolling,frameborder,allowTransparency,allowFullScreen){
-    
+function FacebookLink(domainID, url, width, height, style, scrolling, frameborder, allowFullScreen) {
+    width = width === undefined ? width : 560;
+    height = height === undefined ? height: 434;
+    style = style === undefined ? style : "border:none;overflow:hidden";
+    scrolling = scrolling === undefined ? scrolling : "no";
+    frameborder = frameborder === undefined ? frameborder : "0";
+    //allowTransparency = allowTransparency === undefined ? allowTransparency : "true";
+    allowFullScreen = allowFullScreen === undefined ? allowFullScreen : "true";
+    //
+    var script = new HTMLIFrameElement;
+    script.width = width;
+    script.height = height;
+    script.style = style;
+    script.scrolling = scrolling;
+    script.frameBorder = frameborder;
+    script.allowFullscreen = allowFullScreen;
+    var str = url.split("/");
+    var link;
+    for(var i = str.length-1; i > 0; i--){
+        if(str[i] !== ""){
+            link = str[i];
+            break;
+        }
+    }
+
+    script.src = "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fi%2Fvideos%2F" + link;
+
 }
--- EmbederUsage.js
+++ EmbederUsage.js
@@ -1,3 +1,4 @@
 DefineSite("example1","https://youtube.com/watch?v=3sMkI7BluMQ");
 DefineSite("example2","https://twitter.com/i/status/803861418789335040");
 DefineSite("example3","https://vimeo.com/259539583");
+FacebookLink();
