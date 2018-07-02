fc8efcf72d635e3a74db9101e669f700b7069de2
İbrahim Özdemir <ibrahim.ozdemir@outlook.com.tr>
2018-07-02 17:43:17 +0300

    demo2 initial

--- EmbederUsage.js
+++ EmbederUsage.js
@@ -1,6 +1,14 @@
+//Clearly you could use DefineSite function but that function doesn't contain all features of 
 
 
 
+YoutubeLink("YoutubeExample","https://www.youtube.com/watch?v=UF8uR6Z6KLc",700,900,false,true);
+
+VimeoLink("VimeoExample","https://vimeo.com/164554825",850,800,false,true);
+
+FacebookLink("FacebookExample","https://www.facebook.com/visualstudio/videos/vl.120663884968204/919798388055647/",800,450,false,false,0,false);
+
+DailyMotionLink("DailymotionExample","https://www.dailymotion.com/video/x6ncjpw",900,500,true,false);
 
 
 
